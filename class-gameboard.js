bcdb3fcac14589fd960c3586052ece47d658cdb8
Razvan <76968864+iDanteModo@users.noreply.github.com>
2024-11-14 16:59:37 +0100

    Bugs when setting pieces on X . Y , you can set them on same x or y even if the distance is bigger, bug when you click outside of legal moves the shippieces still increments

--- class-gameboard.js
+++ class-gameboard.js
@@ -4,7 +4,10 @@ export class Gameboard {
         this.ship = ship;
         this.board = Array.from({ length: 10 }, () => {
             return Array(10).fill(0);  // Explicitly return the row
-        })};
+        })
+        this.shipLength = 0 - ship.length; 
+    };
+        
 
     displayBoard() {
         const boardContainer = document.getElementById(`player${this.player}-board`);
@@ -21,14 +24,20 @@ export class Gameboard {
              });
         });
     }
+    ShipPieces() {
+        if(this.shipLength <= 0){
+            this.shipLength ++;
+            return this.shipLength;
+        }
+    }
     placeShip() {
         const cells = document.querySelectorAll('.cell');
         let head = false;
         let headCell =[];
-        let shipPlacementRemaining = 0 - this.ship.length;
+        let piecesRemaining =
         cells.forEach(cell => {
             cell.addEventListener('click',() => {
-                if(!head && shipPlacementRemaining < 0) {   // Starting point of the ship
+                if(!head && this.ShipPieces() <= 0) {   // Starting point of the ship
                     head = true;   // We recognize head of ship has been placed
                     cell.dataset.ship = this.ship.length;  // we make a copy from the head coordinates so we can calculate if placement is legal
                     cell.id = 'ship'; // we color the square to know its our ship 
@@ -36,13 +45,10 @@ export class Gameboard {
                     headCell.push(cell.dataset.row); 
                     headCell.push(cell.dataset.col);
                     console.log(headCell);
-                    shipPlacementRemaining ++;
-                }else if(head = true && shipPlacementRemaining < 0) { // the head has been placed and ship not completed
+                }else if(head = true && this.ShipPieces() <= 0) { // the head has been placed and ship not completed
                         if(cell.dataset.row == headCell[0] || cell.dataset.col == headCell[1]){
                             console.log("LEGAL MOVE");
                             cell.id = 'ship';
-                            shipPlacementRemaining ++;
-                            console.log(shipPlacementRemaining);
                         }
                 }
             })
